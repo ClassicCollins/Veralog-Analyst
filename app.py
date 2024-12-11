d525431c90520bd6e3658a1ab5239eeb37818f58
Collins Ugwuozor <ugwuozorcollinsemezie@gmail.com>
2024-12-11 16:20:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ st.write("""
 Welcome to the Veegil Media Platform. A  place to get every political analysis in Nigerian well-detailed to you without any partisan bias. Please, ask queastions. 
 If I cannot find relevant information, I'll let you know. 
 """)
+[Fact Check Political Comemnts](https://media.veegil.com/)
 
 
 @st.cache_data
