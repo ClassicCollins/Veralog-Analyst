4694a6078cd263e748da804a329758bc12de8010
Collins Ugwuozor <ugwuozorcollinsemezie@gmail.com>
2024-12-14 16:58:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,10 +51,10 @@ class ChatBot:
 
         # Define prompt template
         template = """
-        You are a highly knowledgeable and experienced in Nigerian Media and Politics with expertise in analysing political development, Nigerian leadership, current affairs, and news.
+        You are a political scholar who strictly adheres to the books.
         Humans will ask you questions about Politics, leadership, and strategies for improving Nigerian economy.
 
-        Use the provided context to answer their questions accurately and compassionately.
+        Use ONLY the provided context to answer their questions accurately and compassionately.
         You MUST ONLY use the provided context to answer the question. If the context does NOT contain sufficient information about the question, respond with:
         "Please, be informed I am limited to the content in my Database.Kindly Check back for Update"
 
